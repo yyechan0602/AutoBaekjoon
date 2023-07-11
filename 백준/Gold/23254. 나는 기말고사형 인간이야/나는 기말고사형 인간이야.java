280eba3467e3aa0083864bce9d41988f4caf94da
ariancy <37824506+yyechan0602@users.noreply.github.com>
2023-07-11 11:58:10 +0900

    [Gold V] Title: 나는 기말고사형 인간이야, Time: 584 ms, Memory: 159840 KB -BaekjoonHub

--- 백준/Gold/23254. 나는 기말고사형 인간이야/나는 기말고사형 인간이야.java
+++ 백준/Gold/23254. 나는 기말고사형 인간이야/나는 기말고사형 인간이야.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     private static class test implements Comparable<test> {
@@ -40,28 +39,45 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<test> pq = new PriorityQueue<>();
         int result = 0;
         int[] score = new int[M];
+        List<LinkedList<Integer>> scores = new ArrayList<>();
+
+        for (int i = 0; i < 101; i++) {
+            scores.add(new LinkedList<>());
+        }
 
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < M; i++) {
             score[i] = Integer.parseInt(st.nextToken());
-            result += score[i];
         }
 
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < M; i++) {
-            pq.add(new test(score[i], Integer.parseInt(st.nextToken())));
+            int tempt = Integer.parseInt(st.nextToken());
+            int efficiency = tempt + score[i] <= 100 ? tempt : 100 - score[i];
+            scores.get(efficiency).add(score[i]);
         }
 
+        int pos = 100;
+
         for (int i = 0; i < 24 * N; i++) {
-            test subject = pq.poll();
-            result += subject.efficiency;
-            subject.study();
-            pq.add(subject);
+            while (scores.get(pos).isEmpty()) {
+                pos--;
+            }
+
+            int tempt = scores.get(pos).pop() + pos;
+            int curPos = tempt + pos <= 100 ? pos : 100 - tempt;
+
+            scores.get(curPos).add(tempt);
+        }
+
+        for (int i = 0; i < 101; i++) {
+            while(!scores.get(i).isEmpty()){
+                result += scores.get(i).pop();
+            }
         }
 
         System.out.println(result);
